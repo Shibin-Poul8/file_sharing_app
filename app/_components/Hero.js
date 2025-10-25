0005d45b8b9727396abb9d90f6b29b4fbaf2de25
Abhilash <abhilashks0505@gmail.com>
2025-10-25 15:05:05 +0530

    Hero changed

--- app/_components/Hero.js
+++ app/_components/Hero.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Hero() {
   return (
-    <section className="bg-gray-50 py-20">
+    <section className="fixed inset-0 bg-gray-50 flex items-center z-10">
       <div className="mx-auto max-w-screen-xl px-4 text-center">
         <h1 className="text-4xl font-extrabold text-gray-900 sm:text-5xl lg:text-6xl">
           <span className="text-blue-600">Upload</span>, Save, and{" "}
